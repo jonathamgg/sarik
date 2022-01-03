f826eceb0d61f664db97bde13204efb4572dbea2
Jonathan Gomes <jonathan.tec.ti@gmail.com>
2022-01-03 17:43:44 -0300

    Array pod namespace

--- sarik.sh
+++ sarik.sh
@@ -57,19 +57,22 @@ echo " "
 NODES=`kubectl get node | awk '{print $1}' | grep -v ^NAME` #store nodes
 #for i in "${NODES[@]}";do echo "NODE : $i";done
 
-POD=`kubectl get pod | awk '{print $1}' | grep -v ^NAME` #store pod
+#POD=`kubectl get pod | awk '{print $1}' | grep -v ^NAME` #store pod
 #for i in "${POD[@]}";do echo "POD : $i";done
 
 VALUE_NAMESPACE=`kubectl get namespace | awk '{print $1}' | grep -v ^'default' | grep -v ^NAME | grep -v ^kube` #store name namespace
 #for i in "${LISTNAMES[@]}";do echo "$i";done
 
-POD_NAMESPACE=`kubectl get pod -n "$VALUE_NAMESPACE" | awk '{print $1}' | grep -v ^NAME`
+kubectl get pod -n "$VALUE_NAMESPACE" | awk '{print $1}' | grep -v ^NAME > pod.txt #recebe a lista de pod e joga para o arquivo
+
+readarray POD_NAMESPACE < pod.txt #armazena a lista de pod em um array
 
 CONT=`kubectl get pod -n "$VALUE_NAMESPACE" | awk '{print $1}' | grep -v ^NAME | wc -l`
 #for i in "${POD_NAMESPACE[@]}";do echo "$i";done
-echo "$CONT"
 
-PWD=`pwd`
+#Contadores
+CONT2=6 #Cont progress-bar
+CONT3=0 #Cont pod_namespace
 #
 #--------------------------------------------------------------------------------------#
 
@@ -77,11 +80,13 @@ PWD=`pwd`
 #Docker instalado?
 [ ! -x "$(which docker)" ] && printf "Precisa instalar o docker, por favor, instale.\n" && exit 1
 #minikube instalado?
-[ ! -x "$(which minikube)" ] && printf "Precisa instalar o minikube, por favor, instale.\n" && exit 1
+[ ! -x "$(which minikube)" ] || [ ! -x "$(which kubectl)" ] && printf "Precisa instalar o minikube ou kubectl, por favor, instale.\n" && exit 1
 #--------------------------------------------------------------------------------------#
 
 #---------------------------------FUNÇÕES----------------------------------------------#
 #
+
+#Progress-bar with problem
 progress-bar() {
   local duration=${1}
 
@@ -97,19 +102,64 @@ progress-bar() {
   done
   clean_line
 }
+#Progress-bar ok
+progress-bar2() {
+[[ $# -ne 1 ]] && error 1
+[[ $1 =~ ^[0-9]+$ ]] || error 2
 
+duration=${1}
+barsize=$((`tput cols` - 7))
+unity=$(($barsize / $duration))
+increment=$(($barsize%$duration))
+skip=$(($duration/($duration-$increment)))
+curr_bar=0
+prev_bar=
+for (( elapsed=1; elapsed<=$duration; elapsed++ ))
+do
+  # Elapsed
+prev_bar=$curr_bar
+  let curr_bar+=$unity
+  [[ $increment -eq 0 ]] || {  
+    [[ $skip -eq 1 ]] &&
+      { [[ $(($elapsed%($duration/$increment))) -eq 0 ]] && let curr_bar++; } ||
+    { [[ $(($elapsed%$skip)) -ne 0 ]] && let curr_bar++; }
+  }
+  [[ $elapsed -eq 1 && $increment -eq 1 && $skip -ne 1 ]] && let curr_bar++
+  [[ $(($barsize-$curr_bar)) -eq 1 ]] && let curr_bar++
+  [[ $curr_bar -lt $barsize ]] || curr_bar=$barsize
+  for (( filled=0; filled<=$curr_bar; filled++ )); do
+    printf "▇"
+  done
+
+  # Remaining
+  for (( remain=$curr_bar; remain<$barsize; remain++ )); do
+    printf " "
+  done
+
+  # Percentage
+  printf "| %s%%" $(( ($elapsed*100)/$duration))
+
+  # Return
+  sleep 1
+  printf "\r"
+done
+printf "\n"
+}
 #
 #--------------------------------------------------------------------------------------#
 
 #---------------------------------EXECUÇÃO---------------------------------------------#
 #
-CONT2=5
 until [ $CONT -le 0 ]
 do
-    echo "Faltam $CONT replicas para configurar firewall."
-    echo "Teste kubectl ....."
-    progress-bar $CONT2
+      echo "Faltam $CONT replicas para configurar firewall."
+      VERSION_OS=`kubectl exec -n $VALUE_NAMESPACE ${POD_NAMESPACE[$CONT3]} -- grep ^NAME=.*$ /etc/os-release | awk '{print $1}' | sed s/NAME="."//`
+      echo "$VERSION_OS"
+      echo "$VALUE_NAMESPACE"
+      echo "${POD_NAMESPACE[$CONT3]}"
+      progress-bar2 $CONT2
     ((CONT=CONT-1))
+    ((CONT3=CONT3+1))
 done
 #
 #--------------------------------------------------------------------------------------#
